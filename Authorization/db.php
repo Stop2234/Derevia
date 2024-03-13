411068616ac104585e13597c266d9a854ef88d79
Koltsov Artur <artur-koltsov@list.ru>
2024-03-13 16:25:07 +0300

    init

--- Authorization/db.php
+++ Authorization/db.php
@@ -5,13 +5,13 @@ $username = "admin";
 $password = "admin";
 $dbname = "registeruser";
 
-
+// Соединение с базой данных
 $dbconn = pg_connect("host=$servername dbname=$dbname user=$username password=$password");
 
-
+// Проверка соединения
 if (!$dbconn) {
     die("Connection failed: " . pg_last_error());
 }
 
-
+// Закрываем соединение с базой данных
 pg_close($dbconn);
--- Authorization/register.php
+++ Authorization/register.php
@@ -2,15 +2,11 @@
 
 require_once('Authorization/db.php');
 
-
-$name = $_POST['name'];
-$password = $_POST['password'];
+$name = $_POST['login'];
+$password = $_POST['pass'];
 $repeat_password = $_POST['repeat_password'];
 $email = $_POST['email'];
 
-
-$SQL = "INSERT INTO register (name, password, repeat_password, email) VALUES ($name, $password, $repeat_password, $email)";
-
+$SQL = "INSERT INTO register (name, password, repeat_password, email) VALUES ($name,$password,$repeat_password,$email)";
 
 
-?>
