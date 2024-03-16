756e327f8ee5dfc5e4749cb7d177ed7f2036d48d
Koltsov Artur <artur-koltsov@list.ru>
2024-03-16 11:19:58 +0300

    init

--- Authorization/login.php
+++ Authorization/login.php
@@ -4,28 +4,30 @@
 require_once('db.php');
 
 // Получаем значения логина и пароля из POST-запроса
-$login = $_POST["login"]; // Исправлены скобки на квадратные скобки
-$password = $_POST["password"]; // Исправлены скобки на квадратные скобки
+$login = pg_escape_string($_POST["login"]);
+$password = pg_escape_string($_POST["password"]);
 
 // Проверяем, заполнены ли оба поля
 if (empty($login) || empty($password)) {
     echo "Заполните все поля";
 } else {
     // Формируем SQL-запрос для выборки пользователя из базы данных
-    // Заменены одинарные кавычки вокруг имени таблицы на двойные
-    $SQL = "SELECT * FROM USERS WHERE login = '$login' AND password = '$password'";
+    // Формируем SQL-запрос для выборки пользователя из базы данных
+    $SQL = "SELECT * FROM USERS WHERE login = '$login' AND (password = '$password' OR password IS NULL)";
+
+    var_dump($result);
+    // Выполняем запрос
     $result = pg_query($dbconn, $SQL);
 
-    // Проверяем, есть ли результаты запроса
-    // Исправлены скобки и оператор в pg_num_rows() и pg_fetch_assoc()
-    if (pg_num_rows($result) > 0) {
+    // Проверяем, есть ли результаты запроса и нет ли ошибок
+    if ($result && pg_num_rows($result) > 0) {
         // Если пользователь найден, выводим приветствие
         while ($row = pg_fetch_assoc($result)) {
-            echo "Добро пожаловать " . $row['login'];
+            echo "Добро пожаловать " . htmlspecialchars($row['login']);
         }
     } else {
-        // Если пользователь не найден, выводим сообщение об ошибке
+        // Если пользователь не найден или есть ошибка, выводим сообщение об ошибке
         echo "Нет такого пользователя";
     }
 }
-
+?>
